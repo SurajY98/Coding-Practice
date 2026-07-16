825a89575b6ea6775169ce3021f49ca57a9a87f3
Yadav <suraj.p.yadav@accenture.com>
2026-07-16 11:51:52 +0530

    URL shortner

--- CodeExersise/src/UrlShortener.java
+++ CodeExersise/src/UrlShortener.java
@@ -7,28 +7,30 @@ public class UrlShortener {
     private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     private static final int SHORT_URL_LENGTH = 6;
 
-    Map<String, String> shortToLongMap = new HashMap<>();
-
-    Map<String,String> longToShortMap = new HashMap<>();
+    // URL -> Short Code
+    private static final Map<String, String> urlToCode = new HashMap<>();
+    // Short Code -> URL
+    private static final Map<String,String> codeToUrl = new HashMap<>();
 
     Random random = new Random();
 
     public String shortenUrl(String url){
         String shortCode;
-        if (longToShortMap.containsKey(url)){
-            return longToShortMap.get(url);
+        // If URL is already shortened, return existing code
+        if (urlToCode.containsKey(url)){
+            return urlToCode.get(url);
         }
         do {
             shortCode = generateCode();
-        }while (shortToLongMap.containsKey(shortCode));
-        shortToLongMap.put(shortCode,url);
-        longToShortMap.put(url,shortCode);
+        }while (codeToUrl.containsKey(shortCode));
+        codeToUrl.put(shortCode,url);
+        urlToCode.put(url,shortCode);
         return shortCode;
     }
 
     public String restoreUrl(String shortCode){
-//        return shortToLongMap.get(shortCode);
-        return shortToLongMap.getOrDefault(shortCode,"Url not found");
+//        return codeToUrl.get(shortCode);
+        return codeToUrl.getOrDefault(shortCode,"Url not found");
     }
 
     public String generateCode(){
@@ -44,8 +46,8 @@ public class UrlShortener {
         UrlShortener urlShortener = new UrlShortener();
         String shortUrl = urlShortener.shortenUrl("https://google.com");
         String shortUrl1 = urlShortener.shortenUrl("https://chatgpt.com/g/g-p-6a55f42192848191aed76a886b6e8b58-accenture-interview-questions/c/6a561687-70f4-83ee-8fc2-ed023e9db770");
-        System.out.println(shortUrl1);
+        System.out.println(shortUrl);
 
-        System.out.println(urlShortener.restoreUrl(shortUrl1));
+        System.out.println(urlShortener.restoreUrl(shortUrl));
     }
 }
