05ed36470c904f70c7e409eb5d96a46b5785c89b
Yadav <suraj.p.yadav@accenture.com>
2026-07-16 12:49:36 +0530

    ArraysFrequency

---
+++ CodeExersise/src/ArraysFrequency.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class ArraysFrequency {
+    public static void main(String[] args) {
+        List<Integer> inputArray = Arrays.asList(2,2,1,2,4,3,4,1);
+
+//        Frequency count
+        Map<Integer, Long> frequencyMap =  inputArray.stream().collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+
+//        Sort according to frequency
+        List<Integer> result = inputArray.stream().sorted(
+                (a, b) -> frequencyMap.get(b).compareTo(frequencyMap.get(a))).toList();
+
+        System.out.println(result);
+    }
+}
